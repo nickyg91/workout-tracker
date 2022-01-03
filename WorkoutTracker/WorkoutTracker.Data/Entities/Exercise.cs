a6fa99633089a3dee83603b5f3a74c20d26d2916
Nick Ganter <nickganter91@gmail.com>
2022-01-03 10:03:45 -0600

    Added more tables. Upgraded to .net 6.

---
+++ WorkoutTracker/WorkoutTracker.Data/Entities/Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutTracker.Data.Entities
+{
+    [Table("exercise")]
+    public class Exercise
+    {
+        [Key, Column("id"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(256), Column("name")]
+        public string Name { get; set; }
+        [Column("weight")]
+        public int? Weight { get; set; }
+        [Column("sets")]
+        public byte? Sets { get; set; }
+        [Column("repetitions")]
+        public byte? Repetitions { get; set; }
+        [Column("workout_id")]
+        public int WorkoutId { get; set; }
+        [ForeignKey("WorkoutId")]
+        public Workout Workout { get; set; }
+    }
+}
---
+++ WorkoutTracker/WorkoutTracker.Data/Entities/Workout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WorkoutTracker.Data.Entities
+{
+    [Table("workout")]
+    public class Workout
+    {
+        [Key, Column("id"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("workout_date_utc")]
+        public DateTime WorkoutDateUtc { get; set; }
+        [Column("workout_user_id")]
+        public int WorkoutUserId { get; set; }
+        [ForeignKey("WorkoutUserId")]
+        public WorkoutUser WorkoutUser { get; set; }
+        public ICollection<Exercise> Exercises { get; set; }
+    }
+}
